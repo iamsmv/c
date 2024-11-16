4f1e6d27e9fe2dc6d92068954c81814b610826da
iamsmv <97937923+iamsmv@users.noreply.github.com>
2024-11-16 18:28:33 +0530

    Update rlinkedlis.cpp

--- rlinkedlis.cpp
+++ rlinkedlis.cpp
@@ -52,3 +52,51 @@ int main() {
 
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+Node* reverseLinkedListIterative(Node* head) {
+    Node* prev = nullptr;
+    Node* curr = head;
+    Node* next = nullptr;
+
+    while (curr) {
+        next = curr->next; // Save next node
+        curr->next = prev; // Reverse the link
+        prev = curr;       // Move prev forward
+        curr = next;       // Move curr forward
+    }
+    return prev; // New head of the list
+}
+
+// Helper Functions
+void printList(Node* head) {
+    while (head) {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "NULL\n";
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+
+    cout << "Original List: ";
+    printList(head);
+
+    head = reverseLinkedListIterative(head);
+    cout << "Reversed (Iterative): ";
+    printList(head);
+
+    return 0;
+}
